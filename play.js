1d75537c08ccdd6ae4e2edf49e57b3dce027547a
alisonhussey <alisonhussey@gmail.com>
2020-12-02 23:37:38 +0000

    added input file to hold user input object and exported it to play.js

--- play.js
+++ play.js
@@ -1,17 +1,7 @@
 const { connect } = require('./client');
+
+const { setupInput } = require('./input');
+
 console.log('Connecting ...');
 connect();
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.handleUserInput('data', (key) => {
-    if (key === '\u0003') {
-      process.exit();
-    }
-  });
-  return stdin;
-}
 
